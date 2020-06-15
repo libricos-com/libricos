b04d4600fe2fcd161405bd16e7fd2268e5847585
Jesús <Jess@192.168.1.36>
2020-06-15 19:34:23 +0200

    Pods allow shortcodes

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -18,6 +18,8 @@
  * @package WordPress
  */
 
+define('PODS_SHORTCODE_ALLOW_SUB_SHORTCODES',true);
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'database_name_here' );
