f7a6446b2e9c9710db142667f18506aa6801c6ec
Jesús <Jess@192.168.1.36>
2020-07-04 11:40:22 +0200

    Added custom Javascript
    Function for removing duplicated TOC (table of contents) in POD review template

--- wp-content/themes/twentyeleven-child/functions.php
+++ wp-content/themes/twentyeleven-child/functions.php
@@ -9,3 +9,13 @@ function my_datum($input_date)
 {
 	return date_i18n('Y F d', strtotime($input_date));
 }
+
+/**
+Activada carga de Javascript en el fichero js/custom.js
+@see:
+- https://wordpress.stackexchange.com/questions/306604/adding-javascript-to-child-theme
+*/
+function my_custom_scripts() {
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ),'',true );
+}
+add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
---
+++ wp-content/themes/twentyeleven-child/js/custom.js
@@ -0,0 +1,9 @@
+/**
+Ñapa eliminación de table of contents duplicada en el pods review
+*/
+window.onload = function(){
+	var nodes = document.querySelectorAll("[id='toc_container']");
+	if(nodes.length > 1){
+		nodes[1].remove();
+	}
+};
