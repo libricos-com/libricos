578bbd25431692540567dfe85a1365253d66cefa
Jesús <Jess@192.168.1.36>
2020-07-07 10:03:46 +0200

    Removed toc_container deletion

--- wp-content/themes/twentyeleven-child/js/custom.js
+++ wp-content/themes/twentyeleven-child/js/custom.js
@@ -2,8 +2,10 @@
 Ñapa eliminación de table of contents duplicada en el pods review
 */
 window.onload = function(){
+	/*
 	var nodes = document.querySelectorAll("[id='toc_container']");
 	if(nodes.length > 1){
 		nodes[1].remove();
 	}
+	*/
 };
