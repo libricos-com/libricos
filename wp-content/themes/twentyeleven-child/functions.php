28c3bfd54a5dc17a4e6c8006372e0770cefe7e19
Jesús <Jess@192.168.1.36>
2020-09-10 18:30:48 +0200

    Added shortcode for blog intro get_libro_info

--- wp-content/themes/twentyeleven-child/functions.php
+++ wp-content/themes/twentyeleven-child/functions.php
@@ -42,3 +42,14 @@ add_action( 'pre_get_posts', function ( $q )
         // You can add multiple CPT's separated by comma's
     }
 });
+
+
+/*
+@see: https://www.wpbeginner.com/wp-tutorials/how-to-add-a-shortcode-in-wordpress/
+*/
+add_shortcode( 'get_libro_intro', 'the_dramatist_return_post_id' );
+function the_dramatist_return_post_id() 
+{
+    global $post;
+    return do_shortcode('[pods name="libro" id="'.$post->ID.'" template="Libro Blog Intro"]') ?? '';
+}
