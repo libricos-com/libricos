9670e634422772fdc677a7438a981bc41891fd58
Jesús <Jess@192.168.1.36>
2020-09-12 18:20:34 +0200

    SHORTCODE get_blog_intro improved

--- wp-content/themes/twentyeleven-child/functions.php
+++ wp-content/themes/twentyeleven-child/functions.php
@@ -44,9 +44,11 @@ function my_get_posts( $query )
 Añade shortcode para las entradas de los Libros en el timeline del blog
 @see: https://www.wpbeginner.com/wp-tutorials/how-to-add-a-shortcode-in-wordpress/
 */
-add_shortcode( 'get_libro_intro', 'the_dramatist_return_post_id' );
+add_shortcode( 'get_blog_intro', 'the_dramatist_return_post_id' );
 function the_dramatist_return_post_id() 
 {
     global $post;
-    return do_shortcode('[pods name="libro" id="'.$post->ID.'" template="Libro Blog Intro"]') ?? '';
+    $postType = get_post_type();
+
+    return do_shortcode('[pods name="'.$postType.'" id="'.$post->ID.'" template="Blog Intro '.$postType.'"]') ?? '';
 }
