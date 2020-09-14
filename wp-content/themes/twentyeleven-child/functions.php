738bbd547b153bbe03b8708472207fa211b1b937
Jesús <Jess@192.168.1.36>
2020-09-14 11:26:45 +0200

    Building Blog index
    Creada barra de estado junto al título de cada artículo del blog. Añadiendo foto y texto de expectativas desde el php.

--- wp-content/themes/twentyeleven-child/functions.php
+++ wp-content/themes/twentyeleven-child/functions.php
@@ -41,7 +41,7 @@ function my_get_posts( $query )
 
 
 /*
-Añade shortcode para las entradas de los Libros en el timeline del blog
+Añade shortcode [get_blog_intro] para las entradas de los Libros en el timeline del blog
 @see: https://www.wpbeginner.com/wp-tutorials/how-to-add-a-shortcode-in-wordpress/
 */
 add_shortcode( 'get_blog_intro', 'the_dramatist_return_post_id' );
@@ -52,3 +52,110 @@ function the_dramatist_return_post_id()
 
     return do_shortcode('[pods name="'.$postType.'" id="'.$post->ID.'" template="Blog Intro '.$postType.'"]') ?? '';
 }
+
+
+/*
+@see: https://wordpress.stackexchange.com/questions/39918/wordpress-hooks-filters-insert-before-content-or-after-title
+*/
+function theme_slug_filter_the_content( $content ) 
+{
+	global $post;
+    $postType = get_post_type();
+
+    switch ($postType) {
+    	case 'libro':
+    		$color = 'btn-primary';
+    		$title = 'Ficha técnica del libro';
+    		$icon  = 'fa-book';
+    		break;
+    	case 'review':
+    		$color = 'btn-danger';
+    		$title = 'Esto es un análisis del libro';
+    		$icon  = 'fa-clipboard-list';
+    		break;
+    	case 'nota':
+    		$color = 'btn-warning';
+    		$title = 'Esto es un conjunto de apuntes y pensamientos de un libro';
+    		$icon  = 'fa-pencil-alt';
+    		break;
+    	default:
+    		$color = 'btn-primary-orange';
+    		$title = 'Esto es una entrada del blog';
+    		$icon  = 'fa-rss';
+    		break;
+    }
+
+    $entidad = '<a href="'.get_permalink().'" class="btn btn-lg px-3 '.$color.' mb-2" role="button" data-toggle="tooltip" title="'.$title.' '.get_the_title().'"><i class="fas '.$icon.'"></i> '.ucfirst($postType).'</a>';
+
+
+
+    $portada = get_post_meta(get_the_id(),'portada');
+    $htmlImagen = '';
+     if( !empty($portada[0]['guid']) ){
+     	$htmlImagen = '<div class="text-center"><img src="'.$portada[0]['guid'].'" alt="Imagen de "></div>';
+     }
+
+
+
+    $estado = get_post_meta(get_the_id(),'estado');
+    /*
+	0 | Por leer
+	1 | Siguiente
+	2 | Leído
+	3 | Leyendo
+	4 | Cerrado
+	5 | Pausado
+	6 | No interesado
+	7 | Cuarentena
+    */
+    $estadoHtml = '';
+    if($postType == 'libro'){
+	    if( !empty($estado[0]) ){
+	    	$estado = $estado[0];
+	    }else{
+	    	$estado = '';
+	    }
+		switch ($estado) {
+	    	case 1:
+	    		$color = 'btn-info';
+	    		$icon = 'fa-forward';
+	    		$title = 'A leer próximamente';
+	    		$text = 'Siguiente';
+	    		break;
+	    	case 2:
+	    		$color = 'btn-success';
+	    		$icon = 'fa-check';
+	    		$title = 'Libro leído a la espera de reseña';
+	    		$text = 'Leído';
+	    		break;
+	    	case 3:
+	    		$color = 'btn-warning';
+	    		$icon = 'fa-book-reader';
+	    		$title = 'Leyendo ahora';
+	    		$text = 'Leyendo';
+	    		break;
+	    	
+	    	default:
+	    		$color = 'btn-primary';
+	    		$icon = 'fa-layer-group';
+	    		$title = 'Libro añadido a la biblioteca';
+	    		$text = 'Quiero leer';
+	    		break;
+	    }
+	    $estadoHtml = '<a href="#" class="btn btn-lg px-3 '.$color.' ml-2" role="button" data-toggle="tooltip" title="'.$title.'"><i class="fas '.$icon.'"></i> '.$text.'</a>';
+    }
+
+    $barra = '<div>'.$entidad.$estadoHtml.'</div>';
+   
+    $expectativasHtml = '';
+    if($postType == 'libro'){
+    	$expectativas = get_post_meta(get_the_id(),'expectativas');
+	    if(!empty($expectativas[0])){
+	    	$expectativasHtml = '<p>'.$expectativas[0].'</p>';
+	    }
+	}
+
+    return $barra.$content.$htmlImagen.$expectativasHtml;
+    
+}
+add_filter( 'the_content', 'theme_slug_filter_the_content' );
