cf081605e1ef168ebb9725e6b6b2e11a694cfa0f
Jesús <Jess@192.168.1.36>
2020-09-20 11:22:20 +0200

    Libros page: added list of psicology books (BETA)

--- wp-content/themes/twentyeleven-child/partials/libros.php
+++ wp-content/themes/twentyeleven-child/partials/libros.php
@@ -20,9 +20,39 @@ The template for displaying content in the tpl/libros.php template
 
 <h2>Libros sobre profecías cumplidas</h2>
 
-<h2>Libros sobre amor de pareja</h2>
+<h2>Libros sobre psicología y amor de pareja</h2>
+<?php 
+$args = array(
+    'posts_per_page' => -1,
+    'post_type' => 'libro',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'generos',
+            // 'field' => 'tag_ID',
+            'terms' => 374
+        )
+    )
+);
+$posts = get_posts($args);
+$num = count($posts);
+
+if( ! empty( $posts ) ){
+	?>
+	<ul class="list-group mb-4">
+	<?php
+	foreach ( $posts as $p ){
+		?>
+		<li class="list-group-item">
+			<a href="<?php echo get_permalink($p->ID);?>"><?php echo $p->post_title;?></a>
+		</li>
+		<?php
+	}
+	?>
+	</ul>
+	<?php
+}
+?>
 
 <h2>Libros para sanar el alma (paz y libertad interior)</h2>
-
 <?php echo get_amazon_grid_beta(2);?>
 
