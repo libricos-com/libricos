a36fff94705de67f39febd577c922d60a1652a56
Jesús <Jess@192.168.1.36>
2020-09-20 12:56:32 +0200

    Ficha libros: added listado categoría

--- wp-content/themes/twentyeleven-child/partials/libros.php
+++ wp-content/themes/twentyeleven-child/partials/libros.php
@@ -19,7 +19,6 @@ The template for displaying content in the tpl/libros.php template
 <h2>Libros sobre la palabra de Dios Padre Nuestro (Oración?)</h2>
 <?php 
 // http://192.168.1.44/jesuserro.com/tag/oracion/
-// Género psicologia 374 http://192.168.1.44/jesuserro.com/generos/psicologia
 // taxonomy=post_tag&tag_ID=246&post_type=post
 $args = array(
     'posts_per_page' => -1,
@@ -27,9 +26,7 @@ $args = array(
     'tax_query' => array(
         array(
             'taxonomy' => 'post_tag',
-             // 'field' => 'tag_ID',
              'terms' => 246
-             // 'slug' => 'oracion'
         )
     )
 );
@@ -39,9 +36,7 @@ $args2 = array(
     'tax_query' => array(
         array(
             'taxonomy' => 'post_tag',
-            // 'field' => 'tag_ID',
             'terms' => 246
-            // 'slug' => 'psicologia'
         )
     )
 );
@@ -67,6 +62,8 @@ if( ! empty( $posts ) ){
 ?>
 
 <h2>Libros sobre profecías cumplidas</h2>
+
+<h2>Libros para sanar el alma (paz y libertad interior)</h2>
 
 <h2>Libros sobre psicología y amor de pareja</h2>
 <?php 
@@ -102,6 +99,38 @@ if( ! empty( $posts ) ){
 }
 ?>
 
-<h2>Libros para sanar el alma (paz y libertad interior)</h2>
+<h2>Categoría Libros</h2>
+<?php
+// taxonomy=category&tag_ID=3&post_type=post
+$args = array(
+    'posts_per_page' => -1,
+    'post_type' => 'libro',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'category',
+             'terms' => 3
+        )
+    )
+);
+$posts = get_posts($args);
+$num = count($posts);
+if( ! empty( $posts ) ){
+	?>
+	<ul class="list-group mb-4">
+	<?php
+	foreach ( $posts as $p ){
+		?>
+		<li class="list-group-item">
+			<a href="<?php echo get_permalink($p->ID);?>"><?php echo $p->post_title;?></a> (<?php echo $p->asin;?>)
+		</li>
+		<?php
+	}
+	?>
+	</ul>
+	<?php
+}
+?>
+
+<h2>Libros leídos</h2>
 <?php echo get_amazon_grid_beta(2);?>
 
