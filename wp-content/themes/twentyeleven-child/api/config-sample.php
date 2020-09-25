38e9ba23bb4d8eab60f304185007b3f78ab9fb02
Jesús <Jess@192.168.1.36>
2020-09-25 16:28:10 +0200

    Atacando la api de Goodreads

---
+++ wp-content/themes/twentyeleven-child/api/config-sample.php
@@ -0,0 +1,6 @@
+<?php
+define('JEI_GOODREADS_KEY',     '1234567890123456789012');
+define('JEI_GOODREADS_USER_1',     12345678);
+define('JEI_GOODREADS_USER_2',     87654321);
+define('JEI_GOODREADS_AUTHOR_ID',     123456); // Ignatius of Loyola
+define('JEI_GOODREADS_BOOK_ID',     12345678); // Mero Cristianismo
---
+++ wp-content/themes/twentyeleven-child/api/index.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+@see: https://www.goodreads.com/api/index
+*/
+include 'config.php';
+include '../../../../../goodreads-api/GoodReads.php';
+
+$api = new GoodReads(JEI_GOODREADS_KEY, __DIR__.'/cache');
+
+// $data = $api->getAuthor(JEI_GOODREADS_AUTHOR_ID);
+$data = $api->getBook(JEI_GOODREADS_BOOK_ID);
+// $data = $api->getLatestReads(JEI_GOODREADS_USER_1);
+// $data = $api->getUser(JEI_GOODREADS_USER_1);
+
+// https://www.goodreads.com/user_status/show/JEI_GOODREADS_USER_1?format=xml&key=JEI_GOODREADS_KEY
+// $data = $api->getUserStatus(JEI_GOODREADS_USER_1);
+// $data = $api->getUserStatuses(JEI_GOODREADS_USER_1);
+
+
+print("<pre>".print_r($data,true)."</pre>");
