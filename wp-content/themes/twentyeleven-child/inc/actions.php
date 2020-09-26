a5af823b041b5f0aa95f7cef31c8f6999f25f603
Jesús <Jess@192.168.1.36>
2020-09-26 12:06:52 +0200

    Added CRUD pod save (BETA)

--- wp-content/themes/twentyeleven-child/inc/actions.php
+++ wp-content/themes/twentyeleven-child/inc/actions.php
@@ -15,3 +15,48 @@ function my_custom_scripts() {
     wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ),'',true );
     }
 add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
+
+
+/* 
+@param int $id ID of the post to change, example: get_the_ID()
+@param string $pod_name Nombre del pod a cambiar, example: 'libro'
+@param array $data nuevos datos a introducir, example:
+    $data = array(
+        'name' => 'New book name',
+        'author' => 2,
+        'description' => 'Awesome book, read worthy!'
+    );
+
+@see: 
+- https://pods.io/docs/code/pods/save/
+- https://developer.wordpress.org/reference/hooks/save_post/
+- https://developer.wordpress.org/reference/hooks/post_updated/
+- https://pods.io/docs/code/pods/
+- https://wordpress.org/support/topic/update-pods-fields-by-code/
+
+IMPORTANT: This example skips data sanitization and validation for the sake of simplicity to explain a concept. 
+Do not use as is, always sanitize data from a form.
+
+@example:
+- Set the author (a user relationship field in pod libro) to a user with an ID of 2
+    $pod->save( 'author', 2 );
+- Let's save another book's data.. Save the same data from above, but for the book with an ID of 4
+    $pod->save( $data, null, 4 );
+*/
+function update_pod_campos($id, $pod_name, $data = [])
+{
+    if(empty($id) || empty($pod_name) || empty($data)){
+        return false;
+    }
+   
+    // Get the book item with an ID of 5
+    $pod = pods( $pod_name, $id );
+
+    // Save the data as set above
+    if ( $pod->exists() ) {
+        $pod->save( $data );
+    }
+    
+}
+// add_action( 'save_post', 'update_pod_campos', 1000, 2 );
+add_action( 'post_updated', 'update_pod_campos', 10, 3 );
