876ceff6c0f11621f870816294ffc67a6227aa82
Jesús <Jess@192.168.1.36>
2020-09-29 18:55:10 +0200

    Probando API getReview: no acceso a las private notes

--- wp-content/themes/twentyeleven-child/api/index.php
+++ wp-content/themes/twentyeleven-child/api/index.php
@@ -8,13 +8,16 @@ include '../../../../../goodreads-api/GoodReads.php';
 $api = new GoodReads(JEI_GOODREADS_KEY, __DIR__.'/cache');
 
 // $data = $api->getAuthor(JEI_GOODREADS_AUTHOR_ID);
-$data = $api->getBook(JEI_GOODREADS_BOOK_ID);
+// $data = $api->getBook(JEI_GOODREADS_BOOK_ID);
 // $data = $api->getLatestReads(JEI_GOODREADS_USER_1);
 // $data = $api->getUser(JEI_GOODREADS_USER_1);
 
 // https://www.goodreads.com/user_status/show/JEI_GOODREADS_USER_1?format=xml&key=JEI_GOODREADS_KEY
 // $data = $api->getUserStatus(JEI_GOODREADS_USER_1);
 // $data = $api->getUserStatuses(JEI_GOODREADS_USER_1);
+
+// https://www.goodreads.com/review/show/2312483779
+$data = $api->getReview(2312483779);
 
 
 print("<pre>".print_r($data,true)."</pre>");
