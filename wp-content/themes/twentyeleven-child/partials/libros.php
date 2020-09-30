dfc2c32b4ec8273f90741fdf001a489eb97a57e6
Jesús <Jess@192.168.1.36>
2020-09-30 16:37:44 +0200

    Implemented AAWP grids to libros page

--- wp-content/themes/twentyeleven-child/partials/libros.php
+++ wp-content/themes/twentyeleven-child/partials/libros.php
@@ -2,6 +2,7 @@
 /*
 The template for displaying content in the tpl/libros.php template
 @see: https://docs.pods.io/tutorials/get-values-from-a-relationship-field/
+Ancient code before AAWP @see: https://gist.github.com/jesuserro/9d504d06aaa2094d42d6e8e5a498d45b
 */
 ?>
 
@@ -18,19 +19,12 @@ The template for displaying content in the tpl/libros.php template
 
 <h2>Libros sobre la palabra de Dios Padre Nuestro (Oración?)</h2>
 <?php 
-// http://192.168.1.44/jesuserro.com/tag/oracion/
-// taxonomy=post_tag&tag_ID=246&post_type=post
-$args = array(
-    'posts_per_page' => -1,
-    // 'post_type' => 'libro',
-    'tax_query' => array(
-        array(
-            'taxonomy' => 'post_tag',
-             'terms' => 246
-        )
-    )
-);
-$args2 = array(
+/*
+Tag oración ID 246
+http://192.168.1.44/jesuserro.com/tag/oracion/
+taxonomy=post_tag&tag_ID=246&post_type=post
+*/
+$argsLibros = array(
     'posts_per_page' => -1,
     'post_type' => 'libro',
     'tax_query' => array(
@@ -40,24 +34,17 @@ $args2 = array(
         )
     )
 );
-$posts = get_posts($args);
-$posts2 = get_posts($args2);
-$posts = array_merge($posts, $posts2);
-$num = count($posts);
-if( ! empty( $posts ) ){
-	?>
-	<ul class="list-group mb-4">
-	<?php
-	foreach ( $posts as $p ){
-		?>
-		<li class="list-group-item">
-			<a href="<?php echo get_permalink($p->ID);?>"><?php echo $p->post_title;?></a>
-		</li>
-		<?php
-	}
-	?>
-	</ul>
-	<?php
+$libros = get_posts($argsLibros);
+$num = count($libros);
+$asins = '';
+if( ! empty( $libros ) ){
+	foreach ( $libros as $libro ){
+        if(empty($libro->asin)){
+            continue;
+        }
+        $asins .= $libro->asin.',';
+    }
+    echo do_shortcode('[amazon box="'.rtrim($asins,',').'" grid="3"]');
 }
 ?>
 
@@ -80,22 +67,17 @@ $args = array(
         )
     )
 );
-$posts = get_posts($args);
-$num = count($posts);
-if( ! empty( $posts ) ){
-	?>
-	<ul class="list-group mb-4">
-	<?php
-	foreach ( $posts as $p ){
-		?>
-		<li class="list-group-item">
-			<a href="<?php echo get_permalink($p->ID);?>"><?php echo $p->post_title;?></a>
-		</li>
-		<?php
-	}
-	?>
-	</ul>
-	<?php
+$libros = get_posts($args);
+$asins = '';
+$num = count($libros);
+if( ! empty( $libros ) ){
+	foreach ( $libros as $libro ){
+        if(empty($libro->asin)){
+            continue;
+        }
+        $asins .= $libro->asin.',';
+    }
+    echo do_shortcode('[amazon box="'.rtrim($asins,',').'" grid="3"]');
 }
 ?>
 
@@ -112,22 +94,17 @@ $args = array(
         )
     )
 );
-$posts = get_posts($args);
-$num = count($posts);
-if( ! empty( $posts ) ){
-	?>
-	<ul class="list-group mb-4">
-	<?php
-	foreach ( $posts as $p ){
-		?>
-		<li class="list-group-item">
-			<a href="<?php echo get_permalink($p->ID);?>"><?php echo $p->post_title;?></a> (<?php echo $p->asin;?>)
-		</li>
-		<?php
-	}
-	?>
-	</ul>
-	<?php
+$libros = get_posts($args);
+$asins = '';
+$num = count($libros);
+if( ! empty( $libros ) ){
+	foreach ( $libros as $libro ){
+        if(empty($libro->asin)){
+            continue;
+        }
+        $asins .= $libro->asin.',';
+    }
+    echo do_shortcode('[amazon box="'.rtrim($asins,',').'" grid="3"]');
 }
 ?>
 
