ac18404f34058fbf45b9edf8e82fc54c1a3534d6
Jesús <Jess@192.168.1.36>
2020-10-06 19:10:13 +0200

    Added Notas files (BETA)

---
+++ wp-content/themes/twentyeleven-child/tpl/notas.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: JEI Notas Page 
+
+@see: 
+- https://florianbrinkmann.com/en/organizing-files-functions-wordpress-theme-4190/
+- https://developer.wordpress.org/themes/basics/template-hierarchy/
+- https://wordpress.org/support/topic/providing-templates-with-php/
+*/
+
+get_header(); 
+
+$urlBase = get_site_url();
+?>
+
+<div id="primary">
+	<div id="content" role="main">
+
+        <?php 
+         // Include the page content template.
+        get_template_part( 'partials/notas', 'page' );
+        ?>
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
+
+			<?php // get_template_part( 'content', 'page' ); ?>
+
+			<?php comments_template( '', true ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+
+
+	</div><!-- #content -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
