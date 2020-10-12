8507554e4f329b6c9156eb3a6804af44a4f02485
jesuserro <jesuserro3@gmail.com>
2020-10-12 17:02:48 +0200

    test(SQLs): Added first simple SQLs
    Get pages and get posts by id

---
+++ wp-content/themes/twentyeleven-child/sql/get_page_templates.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM `wp_postmeta` p
+WHERE 
+p.meta_key = '_wp_page_template'
+AND p.meta_value LIKE "%page/%"
+# p.post_id = 13544 #La HOME
+# LIMIT 2;
---
+++ wp-content/themes/twentyeleven-child/sql/get_post_by_id.sql
@@ -0,0 +1,6 @@
+#La página de la HOME
+
+SELECT *
+FROM `wp_posts` p
+WHERE p.id = 13544
+LIMIT 1;
