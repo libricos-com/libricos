9dbe55da4384bcb9ceb8bfa02eee038586952c12
jesuserro <jesuserro3@gmail.com>
2020-10-24 12:50:46 +0200

    refactor(Política de privacidad): Página de privacidad pasada a PHP

---
+++ wp-content/themes/twentyeleven-child/page/politica-privacidad.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: JEI Política de privacidad Page
+
+@see:
+- https://florianbrinkmann.com/en/organizing-files-functions-wordpress-theme-4190/
+- https://developer.wordpress.org/themes/basics/template-hierarchy/
+- https://wordpress.org/support/topic/providing-templates-with-php/
+ */
+echo view('../layout/base', ['page' =>  'politica-privacidad']);
--- wp-content/themes/twentyeleven-child/sql/get_page_templates.sql
+++ wp-content/themes/twentyeleven-child/sql/get_page_templates.sql
@@ -2,6 +2,6 @@ SELECT *
 FROM `wp_postmeta` p
 WHERE 
 p.meta_key = '_wp_page_template'
-AND p.meta_value LIKE "%page/%"
-# p.post_id = 13544 #La HOME
-# LIMIT 2;
+-- AND p.meta_value LIKE "%page/%"
+-- p.post_id = 13544 #La HOME
+-- LIMIT 2;
