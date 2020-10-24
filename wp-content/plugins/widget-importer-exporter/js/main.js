60e1e8590ba34d0ae18ae548bae978582307fc11
jesuserro <jesuserro3@gmail.com>
2020-10-24 17:55:38 +0200

    Remove plugins folder from git "./wp-content/plugins"

--- wp-content/plugins/widget-importer-exporter/js/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function ($) {
-
-    // Show textarea when click "copy and paste"
-    $('#wie-content-toggle').on('click', function () {
-        $('#wie-import-data').fadeIn('fast').focus();
-    });
-
-});
