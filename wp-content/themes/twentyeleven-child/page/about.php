bf7812aadb7cf7b77f96f51e7639410c4944c62c
jesuserro <jesuserro3@gmail.com>
2020-10-25 09:32:11 +0100

    fix(GIT/About page): GIT add all child theme files
    - Added about page to php

---
+++ wp-content/themes/twentyeleven-child/page/about.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: JEI About Page
+
+@see:
+- https://afiliados.amazon.es/help/operating/compare?ref_=pe_307821_517477581
+
+*/
+echo view('../layout/base', ['page' =>  'about']);
