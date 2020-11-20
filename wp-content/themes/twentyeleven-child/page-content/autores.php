5bddba9a9bc02b13bd005451736fa7a0ee106d5c
jesuserro <jesuserro3@gmail.com>
2020-11-20 15:24:36 +0100

    style(Autores page): Ajuste HTML grid
    - se tiene en cuenta file row con 3 elementos.
    - ajustes anchura con breakpoints.
    - en tablet no se ajusta bien.

--- wp-content/themes/twentyeleven-child/page-content/autores.php
+++ wp-content/themes/twentyeleven-child/page-content/autores.php
@@ -12,30 +12,35 @@ if ( have_posts() ) {
     twentyeleven_content_nav( 'nav-above' );
     ?>
     <div class="container">
-        <div class="row">
-            <?php
-            while(have_posts()){
-                the_post(); 
-                // the_content();
-                // get_template_part( 'content', get_post_format() ); 
-                $id = get_the_ID();
-                $titulo = get_the_title();
-                $url = esc_url( get_permalink( $id ) );
-                $pod = pods( 'autor', $id );
-                $portada = $pod->field( 'portada' );
-                $portada = wp_get_attachment_image_src($portada['ID'], 400)[0];
-                ?>
-
-                <div class="col-md-4 col-xs-4 mb-5 text-center">
+        <?php
+        $i = 0;
+        $numOfCols = 3;
+        while(have_posts()){
+            the_post(); 
+            // the_content();
+            // get_template_part( 'content', get_post_format() ); 
+            $id = get_the_ID();
+            $titulo = get_the_title();
+            $url = esc_url( get_permalink( $id ) );
+            $pod = pods( 'autor', $id );
+            $portada = $pod->field( 'portada' );
+            $portada = wp_get_attachment_image_src($portada['ID'], 400)[0];
+            if($i%3 == 0) {
+                echo '<div class="row mb-5">';
+            }
+            ?>
+                <div class="col-sm-4 col-md-4 col-lg-4 text-center">
                     <a href="<?php echo $url;?>"><img class="rounded-circle z-depth-2 circlex" alt="Imagen del autor <?php echo $titulo;?>" src="<?php echo $portada;?>"
                     data-holder-rendered="true"></a>
                     <strong><?php echo $titulo;?></strong>
                 </div>
-
-                <?php
+            <?php
+            $i++;
+            if($i%3 == 0) {
+                echo '</div>';
             }
-            ?>
-        </div>
+        }
+        ?>    
     </div>
     <?php
     twentyeleven_content_nav( 'nav-below' );
