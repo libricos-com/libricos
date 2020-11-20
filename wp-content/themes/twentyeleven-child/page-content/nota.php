9c01b9ab4ef82e9616cb0d6af6c74dbe2c244a85
jesuserro <jesuserro3@gmail.com>
2020-11-20 19:31:29 +0100

    feat(Nota single): Added nota single to php
    Se quita el html p de los textos largos en los single de Nota y Review

--- wp-content/themes/twentyeleven-child/page-content/nota.php
+++ wp-content/themes/twentyeleven-child/page-content/nota.php
@@ -8,11 +8,9 @@
     $url = esc_url( get_permalink( $id ) );
 
     $texto = get_post_meta($id,'contenido')[0];
-    $puntuacion = get_post_meta($id,'rating')[0];
-    $goodreads_url = get_post_meta($id,'goodreads_url')[0];
-    
+  
     //get Pods object for current post
-    $pod = pods( 'review', $id );
+    $pod = pods( 'nota', $id );
 
     $libro = $pod->field( 'libro' );
     $asin = get_post_meta($libro['ID'],'asin')[0];
@@ -27,19 +25,13 @@
 
 <h1><?php echo $post_title;?></h1>
 
-<strong>Fecha reseña</strong>: <?php echo $fecha;?> 
+<strong>Fecha publicación</strong>: <?php echo $fecha;?> 
 
 <div class="text-center mb-4">
     <a href="<?php echo $libro_link;?>"><img src="<?php echo $src[0];?>" alt="Portada del libro <?php echo $titulo;?>" class="img-fluid" /></a>
-    <div><?php // echo get_kkstarring();?></div>
 </div>
 
 <?php echo $texto;?>
-
-<ul>
-    <li><strong>Mi puntuación</strong>: <?php echo $puntuacion;?></li>
-    <li><a href="<?php echo $goodreads_url;?>">Reseña en Goodreads</a></li>
-</ul>
 
 <hr />
 <div class="text-center p-2">
---
+++ wp-content/themes/twentyeleven-child/single-nota.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: JEI nota detail page
+@see: 
+- https://florianbrinkmann.com/en/organizing-files-functions-wordpress-theme-4190/
+- https://wordpress.org/support/topic/providing-templates-with-php/
+- https://docs.pods.io/tutorials/get-values-from-a-relationship-field/
+*/
+echo view('../layout/base', ['page' =>  'nota']);
