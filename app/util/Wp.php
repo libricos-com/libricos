1ed520c6dca636e4459be956a681a4fb3d5dec93
jesuserro <jesuserro3@gmail.com>
2020-11-25 16:30:12 +0100

    feat(PHP Classes): Added Utils

---
+++ app/util/Wp.php
@@ -0,0 +1,91 @@
+<?php
+namespace App\Util;
+/**
+ * Wp class
+ * 
+ */
+abstract class Wp
+{
+ 
+    public static function get_books_by_category_id($term_id)
+    {
+        $args = array(
+            'posts_per_page' => -1,
+            'post_type' => 'libro',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'category',
+                    'terms' => $term_id
+                )
+            )
+        );
+        return get_posts($args);
+    }
+
+    public static function get_books_by_genero_id($term_id)
+    {
+        $args = array(
+            'posts_per_page' => -1,
+            'post_type' => 'libro',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'genero',
+                    'terms' => $term_id
+                )
+            )
+        );
+        return get_posts($args);
+    }
+
+    public static function get_books_by_tag_id($term_id)
+    {
+        $args = array(
+            'posts_per_page' => -1,
+            'post_type' => 'libro',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'post_tag',
+                    'terms' => $term_id
+                )
+            )
+        );
+        return get_posts($args);
+    }
+
+
+    /**
+     * Caja Amazon class Libros
+     * Captura las últimas reviews y coge los asins y los ids del libro correspondiente 
+     *
+     * @param string[] $input
+     * @return string[ string $asins, string $ids ]
+     */
+    public static function get_libros_asins($libros)
+    {
+        $asins = $ids = '';
+        if( ! empty( $libros ) ){
+            foreach ( $libros as $libro ){
+                if(empty($libro->asin)){
+                    continue;
+                }
+                $asins .= $libro->asin.',';
+                $ids .= $libro->ID.',';
+            }
+            
+            // Remove duplicate ids 
+            $asins = implode(',', array_unique(explode(',', $asins)));
+            $ids = implode(',', array_unique(explode(',', $ids)));
+
+            // and remove last comma
+            $asins = rtrim($asins,',');
+            $ids = rtrim($ids,',');
+        }
+
+        return [
+            $asins, 
+            $ids
+        ];
+    }
+
+}
+
--- wp-content/themes/twentyeleven-child/page-content/libros.php
+++ wp-content/themes/twentyeleven-child/page-content/libros.php
@@ -1,22 +1,22 @@
 <?php 
 /*
 The template for displaying content in the tpl/libros.php template
+@see: https://stackoverflow.com/questions/44219423/php-fatal-error-cannot-instantiate-abstract-class
+@see: https://stackoverflow.com/questions/53895044/clarifying-uml-class-diagram-of-factory-method-design-pattern
 @see: https://docs.pods.io/tutorials/get-values-from-a-relationship-field/
-Ancient code before AAWP @see: https://gist.github.com/jesuserro/9d504d06aaa2094d42d6e8e5a498d45b
 
 Ideas/más buscadas:
 <h2>Libros sobre el sentido del sufrimiento</h2>
 <h2>Libros sobre la palabra de Dios Padre Nuestro (Oración?)</h2>
 */
-use App\Entity\BookAmazonFactory;
-$libro = BookAmazonFactory::create();
+use App\Util\Wp;
 ?>
 
 <h1>Últimos libros</h1>
 <?php
-$libros = $libro->get_books_by_category_id(3);
-$asins = $libro->get_libros_asins($libros)[0];
-$ids = $libro->get_libros_asins($libros)[1];
+$libros = Wp::get_books_by_category_id(3);
+$asins = Wp::get_libros_asins($libros)[0];
+$ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="3" template="my-vertical"]');
 ?>
 
@@ -25,9 +25,9 @@ echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,','
 emocional, ansiedad, autoestima, depresión, imprescindibles. Libros cristianos a buen precio, español y Kindle.</p>
 <?php 
 // Género autoayuda 410 http://192.168.1.44/jesuserro.com/generos/autoayuda
-$libros = $libro->get_books_by_genero_id(410);
-$asins = $libro->get_libros_asins($libros)[0];
-$ids = $libro->get_libros_asins($libros)[1];
+$libros = Wp::get_books_by_genero_id(410);
+$asins = Wp::get_libros_asins($libros)[0];
+$ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="3" template="my-vertical"]');
 ?>
 
@@ -35,9 +35,9 @@ echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,','
 <p>Libros que cuentan historias para jóvenes, hombres, mujeres, niños.</p>
 <?php 
 // Género testimonios http://192.168.1.44/jesuserro.com/generos/psicologia
-$libros = $libro->get_books_by_genero_id(426);
-$asins = $libro->get_libros_asins($libros)[0];
-$ids = $libro->get_libros_asins($libros)[1];
+$libros = Wp::get_books_by_genero_id(426);
+$asins = Wp::get_libros_asins($libros)[0];
+$ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="3" template="my-vertical"]');
 ?>
 
@@ -48,25 +48,25 @@ Tag oración ID 246
 http://192.168.1.44/jesuserro.com/tag/oracion/
 taxonomy=post_tag&tag_ID=246&post_type=post
 */
-$libros = $libro->get_books_by_tag_id(246);
-$asins = $libro->get_libros_asins($libros)[0];
-$ids = $libro->get_libros_asins($libros)[1];
+$libros = Wp::get_books_by_tag_id(246);
+$asins = Wp::get_libros_asins($libros)[0];
+$ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="3" template="my-vertical"]');
 ?>
 
 <h2>Libros sobre profecías cumplidas</h2>
 <?php 
-$libros = $libro->get_books_by_genero_id(434);
-$asins = $libro->get_libros_asins($libros)[0];
-$ids = $libro->get_libros_asins($libros)[1];
+$libros = Wp::get_books_by_genero_id(434);
+$asins = Wp::get_libros_asins($libros)[0];
+$ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="3" template="my-vertical"]');
 ?>
 
 <h2>Libros sobre psicología y amor de pareja</h2>
 <?php 
-$libros = $libro->get_books_by_genero_id(405);
-$asins = $libro->get_libros_asins($libros)[0];
-$ids = $libro->get_libros_asins($libros)[1];
+$libros = Wp::get_books_by_genero_id(405);
+$asins = Wp::get_libros_asins($libros)[0];
+$ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="3" template="my-vertical"]');
 ?>
 
