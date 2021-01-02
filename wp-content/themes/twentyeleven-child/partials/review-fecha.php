c2cbd5f907134592ed9beba5350394bbce4fecfb
jesuserro <jeigatwork@gmail.com>
2021-01-02 16:25:13 +0100

    feat(Reviews): Review dates on right

--- wp-content/themes/twentyeleven-child/partials/review-fecha.php
+++ wp-content/themes/twentyeleven-child/partials/review-fecha.php
@@ -1,4 +1,4 @@
-<div>
+<div class="review-date">
     <span class="badge badge-secondary">
         <i class="far fa-calendar-check" aria-hidden="true"></i> 
     </span> <?php echo $this2->get_post_date();?>
