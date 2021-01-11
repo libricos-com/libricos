3191894ead9ca73b23f5fc4481ebdebaa22132c1
jesuserro <jeigatwork@gmail.com>
2021-01-11 12:55:03 +0100

    feat(Autor): chip template with author image.

--- wp-content/themes/twentyeleven-child/partials/libro-autores.php
+++ wp-content/themes/twentyeleven-child/partials/libro-autores.php
@@ -6,10 +6,20 @@ if(!empty($this2->autores)){
         <ul class="list-inline-pipe">
         <?php
         foreach($this2->autores as $autor){
-            ?>
+            $img = get_the_post_thumbnail_url($autor['ID'],'thumbnail');
+            $pod = pods("autor", $autor['ID']);
+            $portada = $pod->field( 'portada' );
+            $img = wp_get_attachment_image_src($portada['ID'], 200)[0];
+        ?>
             <li class="list-inline-item">
+                <div class="chip align-bottom">
+                    <a href="<?php echo esc_url( get_permalink( $autor['ID'] ) );?>" target="blank" alt="<?php echo $autor['post_title'];?>">
+                        <img src="<?php echo $img;?>" alt="<?php echo $autor['post_title'];?>" title="<?php echo $autor['post_title'];?>">
+                    </a> 
+                </div>
                 <a href="<?php echo esc_url( get_permalink( $autor['ID'] ) );?>" alt=""><?php echo $autor['post_title'];?></a>
             </li>
+
             <?php
         }
         ?>
