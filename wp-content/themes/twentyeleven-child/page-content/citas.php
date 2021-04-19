5028fdb1b1447a3d54d8037e1b0fd0113fa3a11f
jesuserro <jeigatwork@gmail.com>
2021-04-19 12:27:21 +0200

    feat(Citas page): Added citas page through archive

---
+++ wp-content/themes/twentyeleven-child/page-content/citas.php
@@ -0,0 +1,49 @@
+<?php 
+/*
+The template for displaying content in the tpl/libros.php template
+@see: https://developer.wordpress.org/themes/template-files-section/taxonomy-templates/
+*/
+use App\Entity\Quote;
+
+$tamano_grid = 4;
+$asins = $ids = '';
+$term = get_queried_object();
+?>
+
+<h2>Todas las citas</h2>
+<div id="citas" class="row pl-3 pr-3 jei-amz-grd">
+    <?php
+    while ( have_posts() ) : the_post();
+        $post       = get_post();
+        $Quote      = new Quote($post);
+        $cita       = $Quote->getCita();
+        $citatags   = $Quote->getCitatags();
+        $book       = $Quote->getBook();
+        $longTitle  = $Quote->getLibroLongTitle();
+        $shortTitle = $Quote->getLibroShortTitle();
+        $autorName  = $Quote->getAutorName();
+        $asin       = $Quote->getAsin();
+        $libroUrl   = esc_url( get_permalink( $book['ID'] ) );
+        $autorUrl   = esc_url( get_permalink( $Quote->getAutorId() ) );
+
+        echo view('../partials/quote-sell', [
+            'cita'        => $cita, 
+            'autorName'   => $autorName,
+            'tituloLibro' => $longTitle,
+            'shortTitle'  => $shortTitle,
+            'citatags'    => $citatags,
+            'asin'        => $asin,
+            'libroUrl'    => $libroUrl,
+            'autorUrl'    => $autorUrl
+            ]
+        );
+    endwhile; 
+    ?>
+
+    <h2>Novedades Amazon</h2>
+    <div class="jei-amz-grd">
+        <?php echo do_shortcode('[amazon template="vertical" grid="'.$tamano_grid.'" new="mejores libros" items="24"]');?>
+    </div>
+
+</div>
+
