a3f62b6567770bcc21a1748becdb2178ebcca7ab
jesuserro <jeigatwork@gmail.com>
2021-04-22 13:02:04 +0200

    feat(Home): Adjusting grid size to 4 items

--- wp-content/themes/twentyeleven-child/page-content/home.php
+++ wp-content/themes/twentyeleven-child/page-content/home.php
@@ -12,7 +12,7 @@ $posts = get_posts(
 ?>
 <h2><a href="./biblioteca">Últimas recomendaciones</a></h2>
 <?php
-$libros = Wp::get_books_by_category_id(3, 4);
+$libros = Wp::get_books_by_category_id(3, $tamano_grid);
 $asins = Wp::get_libros_asins($libros)[0];
 $ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="'.$tamano_grid.'" template="my-vertical"]');
@@ -40,7 +40,7 @@ echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,','
 
 <h2><a href="<?php echo get_tag_link(470);?>">Libros de cocina</a></h2>
 <?php 
-$libros = Wp::get_books_by_tag_id(470, 4);
+$libros = Wp::get_books_by_tag_id(470, $tamano_grid);
 $asins = Wp::get_libros_asins($libros)[0];
 $ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="'.$tamano_grid.'" template="my-vertical"]');
@@ -53,7 +53,7 @@ echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,','
 emocional, ansiedad, autoestima, depresión, imprescindibles.</p>
 <?php 
 // Género autoayuda 410 http://192.168.1.44/jesuserro.com/generos/autoayuda
-$libros = Wp::get_books_by_genero_id(410, 4);
+$libros = Wp::get_books_by_genero_id(410, $tamano_grid);
 $asins = Wp::get_libros_asins($libros)[0];
 $ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="'.$tamano_grid.'" template="my-vertical"]');
@@ -63,7 +63,7 @@ echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,','
 
 <h2><a href="<?php echo get_term_link(405, 'genero');?>">Libros sobre psicología y amor de pareja</a></h2>
 <?php 
-$libros = Wp::get_books_by_genero_id(405, 4);
+$libros = Wp::get_books_by_genero_id(405, $tamano_grid);
 $asins = Wp::get_libros_asins($libros)[0];
 $ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="'.$tamano_grid.'" template="my-vertical"]');
@@ -73,7 +73,7 @@ echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,','
 
 <h2><a href="<?php echo get_tag_link(275);?>">Libros sobre profecías cumplidas</a></h2>
 <?php 
-$libros = Wp::get_books_by_tag_id(275, 4);
+$libros = Wp::get_books_by_tag_id(275, $tamano_grid);
 $asins = Wp::get_libros_asins($libros)[0];
 $ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="'.$tamano_grid.'" template="my-vertical"]');
@@ -84,7 +84,7 @@ echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,','
 <h2><a href="<?php echo get_tag_link(344);?>">Libros sobre historias y testimonios</a></h2>
 <p>Libros que cuentan historias de cambio para jóvenes, hombres, mujeres, niños.</p>
 <?php 
-$libros = Wp::get_books_by_tag_id(344, 4);
+$libros = Wp::get_books_by_tag_id(344, $tamano_grid);
 $asins = Wp::get_libros_asins($libros)[0];
 $ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="'.$tamano_grid.'" template="my-vertical"]');
@@ -99,7 +99,7 @@ Tag oración ID 246
 http://192.168.1.44/jesuserro.com/tag/oracion/
 taxonomy=post_tag&tag_ID=246&post_type=post
 */
-$libros = Wp::get_books_by_tag_id(246, 4);
+$libros = Wp::get_books_by_tag_id(246, $tamano_grid);
 $asins = Wp::get_libros_asins($libros)[0];
 $ids = Wp::get_libros_asins($libros)[1];
 echo do_shortcode('[amazon box="'.rtrim($asins,',').'" tpl_ids="'.rtrim($ids,',').'" grid="'.$tamano_grid.'" template="my-vertical"]');
